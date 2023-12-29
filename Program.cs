19f8450e7d3b110af14690cffdb8c217b1df530a
Román Reséndiz Trejo <114356774+Roman-ux-lang@users.noreply.github.com>
2023-12-29 00:25:49 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using Domain;
+using Infrastructure;
+
+var _connectionString = "Data Source=**;Initial Catalog=***;User ID=***;Password=***;";
+var _empleadosDbContext = new EmpleadosDbContext(_connectionString);
+
+var empleado = new Empleado
+{
+    Id = Guid.NewGuid(),
+    Nombre = "Prueba 999999",
+    Edad = 99
+};
+_empleadosDbContext.Create(empleado);
+
+var list = _empleadosDbContext.List();
+foreach (var e in list)
+{
+    Console.Write($"\tId: {e.Id} \tNombre: {e.Nombre} \tEdad: {e.Edad}\n");
+}
